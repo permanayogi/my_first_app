00ff454c9622efe2785ce4ea1f0edb371f9f0f6f
permanayogi <yogipermana597@gmail.com>
2025-02-04 11:38:24 +0700

    swipe to delete item and undo from snackbar

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ListScreen extends StatelessWidget {
+class ListScreen extends StatefulWidget {
+  //change stateless to stateful
   const ListScreen({super.key});
-  static const List<String> items = [
+
+  @override
+  _ListScreenState createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  final List<String> items = [
     //create list for items
     'Apple',
     'Banana',
@@ -10,6 +17,13 @@ class ListScreen extends StatelessWidget {
     'Mango',
     'Grapes',
   ];
+
+  void _deleteItem(int index) {
+    //method for delete item
+    setState(() {
+      items.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +34,40 @@ class ListScreen extends StatelessWidget {
       body: ListView.builder(
           itemCount: items.length,
           itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(items[index]),
+            return Dismissible(
+              //swipe right to left delete item
+              key: Key(items[index]),
+              background: Container(
+                color: Colors.red,
+                alignment: Alignment.centerRight,
+                padding: const EdgeInsets.only(right: 20),
+                child: const Icon(Icons.delete, color: Colors.white),
+              ),
+              direction: DismissDirection
+                  .endToStart, //direction to swipe (Right to left)
+              onDismissed: (direction) {
+                //run method on dismiss
+                final deletedItem = items[index]; //store deleted item
+                _deleteItem(index); //Remove item
+
+                Future.delayed(const Duration(milliseconds: 100), () {
+                  //add delay 100ms
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text('$deletedItem deleted'),
+                    action: SnackBarAction(
+                        label: 'Undo',
+                        onPressed: () {
+                          setState(() {
+                            items.insert(
+                                index, deletedItem); //undo deleted item
+                          });
+                        }),
+                  ));
+                });
+              },
+              child: ListTile(
+                title: Text(items[index]),
+              ),
             );
           }),
     );
